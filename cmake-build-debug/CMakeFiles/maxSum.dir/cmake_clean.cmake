a34b57339f927e1010626ee76c9e091950757afa
Anton Fenske <anton.fenske@klarna.com>
2019-12-18 22:13:43 +0100

    First commit

---
+++ cmake-build-debug/CMakeFiles/maxSum.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/maxSum.dir/main.cpp.o"
+  "CMakeFiles/maxSum.dir/maxRowCol.cpp.o"
+  "maxSum"
+  "maxSum.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/maxSum.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/maxRowCol.h
@@ -0,0 +1,9 @@
+#ifndef MaxRowCol
+#define MaxRowCol
+#define N 3
+#include <tuple>
+using namespace std;
+
+tuple<int, int> getMaxRow(int arr[][N]);
+tuple<int, int> getMaxCol(int arr[][N]);
+#endif
